378bedb36eea1783aa858518b3f0d59c28c702f1
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-25 09:39:57 +0600

    Vercel deploy completed

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// const categories = require("./categories.json")
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ltefwui.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
