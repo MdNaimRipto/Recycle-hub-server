ea3739b6b2c60aec8952374919a762fba2f1e0e6
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-26 18:26:20 +0600

    User put function created

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require("cors")
 const app = express()
 require("dotenv").config()
+const jwt = require("jsonwebtoken")
+
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Access Forbidden" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ltefwui.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,12 +34,53 @@ const run = async () => {
     try {
         const categoriesCollection = client.db('recycleHub').collection("categories")
         const carsCollection = client.db('recycleHub').collection("cars")
+        const usersCollection = client.db('recycleHub').collection("users");
+
+
+        // Admin verify:
+        const verifyAdmin = async (req, res, next) => {
+            const decodedEmail = req.decoded.email
+            const query = { email: decodedEmail }
+            const user = await usersCollection.findOne(query)
+            if (user?.role !== "admin") {
+                return res.status(403).send({ message: "Forbidden access" })
+            }
+            next()
+        }
+
+        // Seller verify:
+        const verifySeller = async (req, res, next) => {
+            const decodedEmail = req.decoded.email
+            const query = { email: decodedEmail }
+            const user = await usersCollection.findOne(query)
+            if (user?.role !== "seller") {
+                return res.status(403).send({ message: "Forbidden access" })
+            }
+            next()
+        }
+
+        // Jwt token:
+
+        app.get("/jwt", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "5h" })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ message: "Unauthorized Access" })
+        })
+
+        // category section:
 
         app.get("/categories", async (req, res) => {
             const query = {};
             const categories = await categoriesCollection.find(query).toArray()
             res.send(categories)
         })
+
+        // cars data section:
 
         app.get("/allCars", async (req, res) => {
             const query = {};
@@ -35,11 +94,36 @@ const run = async () => {
             res.send(cars)
         })
 
+        app.get("/carDetails/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const cars = await carsCollection.findOne(query)
+            res.send(cars)
+        })
+
+        // Cars by category_id:
+
         app.get("/categoryItem/:category_id", async (req, res) => {
             const category_id = req.params.category_id;
             const query = { category_id: category_id }
             const cars = await carsCollection.find(query).toArray()
             res.send(cars)
+        })
+
+        // Users Section:
+
+        app.put("/users", async (req, res) => {
+            const user = req.body.user;
+            const filter = { email: user?.email }
+            console.log(filter)
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    user: user
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
         })
     }
     finally {
